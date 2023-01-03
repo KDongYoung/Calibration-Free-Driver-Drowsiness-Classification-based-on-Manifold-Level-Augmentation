3db1ea9d430ca1cbc99e743d266a2a3186453d18
DongYoung Kim <37610908+KDongYoung@users.noreply.github.com>
2023-01-04 00:28:45 +0900

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,8 @@
-echo "Public dataset"
+echo "Dataset B"
 for idx in 0 1 2 3 4 5 6 7 # GPU IDX
 do
     gpu_idx=$(($idx%4))                                                                                                                                                                                                                                                                              
-    CUDA_VISIBLE_DEVICES=$gpu_idx python code정리/TotalMain.py --subject_group=$idx --cuda_num=$gpu_idx --dataset_name=SA&
+    CUDA_VISIBLE_DEVICES=$gpu_idx python code정리/TotalMain.py --subject_group=$idx --cuda_num=$gpu_idx --dataset_name=B&
 done
 wait
 echo "Public dataset End"
